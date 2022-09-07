f412271c52ac64d6762cfe206057eb0e5131e4f8
ayka <aymirzajumatashov@gmail.com>
2022-09-07 19:18:18 +0500

    добавил Entry и одна Кнопка

---
+++ example.py
@@ -0,0 +1,11 @@
+
+
+data = {
+    "languages1":"Python",
+    "languages2":"Java",
+    "languages3":"CSharp"
+}
+
+for key, value in data.items():
+    print(f"Phone:{key} Lang:{value}")
+
--- form_0001.py
+++ form_0001.py
@@ -15,16 +15,29 @@ def open_win():
 
 def show_message():
     messagebox.showinfo("Gui Python",message.get())
+def saveDataToJson():
+    messagebox.showinfo("Gui Python", data)
+
 
 message = StringVar()
 
+data = {"languages":"Python","languages":"Java","languages":"CSharp"}
+
 message_entry = Entry(textvariable=message)
-message_entry.place(relx=.5,rely=.1, anchor="c")
+message_entry.place(x=250,y=40, anchor="c")
+
+entry1 = Entry(textvariable=message)
+entry1.place(x=250,y=75, anchor="c")
+
+entry1 = Entry(textvariable=message)
+entry1.place(x=250,y=110, anchor="c")
 
 btn1 = ttk.Button(text="Click Me", command=show_message)
-btn1.place(x=30,y=30)
+btn1.place(x=20,y=35)
 btn2 = ttk.Button(window,text="Open New Window", command=open_win)
-btn2.place(x=30,y=65)
+btn2.place(x=20,y=65)
+btn2 = ttk.Button(window,text="Save to Json", command=open_win)
+btn2.place(x=20,y=95)
 
 
 window.mainloop()
